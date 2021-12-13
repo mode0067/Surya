a292a9135ee4a230e7a23b27276c7aa1fc605895
mode0067 <95365450+mode0067@users.noreply.github.com>
2021-12-13 14:26:38 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,13 @@
 let menu = document.querySelector('#menu-btn');
 let header = document.querySelector('.header');
-
 menu.onclick = () =>{
     menu.classList.toggle('fa-times');
     header.classList.toggle('active');
 }
-
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
     header.classList.remove('active');
 }
-
 let themeToggler = document.querySelector('#theme-toggler');
 
 themeToggler.onclick = () =>{
