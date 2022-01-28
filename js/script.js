2b510105efb02c40c397ed5d9869dd513a0c4c60
mode0067 <95365450+mode0067@users.noreply.github.com>
2022-01-28 17:52:12 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+var preloder = document.getElementById('Loading');
+function myFunction(){
+    preloder.style.display = 'none';
 let menu = document.querySelector('#menu-btn');
 let header = document.querySelector('.header');
 menu.onclick = () =>{
